783769468e9b600057b7d37a11e6823826792c8b
Ivan B <ivanbolderoff@gmail.com>
2021-01-14 16:52:35 +1030

    fixed turns for and addded cs, needs refactoring

--- app.js
+++ app.js
@@ -1,22 +1,84 @@
-import game from "./modules/game.js"
-import compPlayer from "./modules/ai.js" // hmmm
+import game from "./modules/game.js";
+import compPlayer from "./modules/ai.js"; // hmmm
 
-const gameBox = document.querySelectorAll(".gameContainer") ; 
-const squares = document.querySelectorAll(".square")
-console.log(squares)
-const startButton = document.querySelector("#startGame")
-const aiCheck = document.querySelector("#PVPorPVE") 
+const gameBox = document.querySelectorAll(".gameContainer");
+const squares = document.querySelectorAll(".square");
+console.log(squares);
+const startButton = document.querySelector("#startGame");
+const aiCheck = document.querySelector("#PVPorPVE");
+const squareNew = document.querySelectorAll(".squareActive");
 
-const {theGame} = game() ; 
+const [
+    board,
+    winConditions,
+    PVPorPVE,
+    startGame,
+    turnClick,
+    turn,
+    checkIfGameOver,
+    emptyIndicies,
+] = game();
 
-startButton.addEventListener("click", ()=>{
-    squares.forEach(square => {
-        square.className = "squareActive"
-    })
-
-    if(aiCheck.checked){
-        game().startGame(squares, "PVE") ; 
-    }else{
-        game().startGame(squares, "PVP")
+function stuff(player, altPlayer) {
+    function togglePlayer(player1, player2) {
+        let currentPlayer = player1;
+        if (player1.marker === "X") {
+            currentPlayer = player2;
+            console.log(currentPlayer);
+            return currentPlayer;
+        } else {
+            currentPlayer = player1;
+            console.log(currentPlayer);
+            return currentPlayer;
+        }
     }
-})
+
+    console.log(player.marker, togglePlayer(player, altPlayer));
+    for (let i = 0; i < squares.length; i++) {
+        if (board[i] == "e") {
+            squares[i].addEventListener("click", (square) => {
+                console.log(squares[i], squares[i].innerHTML);
+                squares[i].innerHTML = `${player.marker}`; // X or O
+                squares[i].disabled = true;
+                board[i] = player.marker;
+                console.log(board);
+
+                let temp = player;
+                player = altPlayer;
+                altPlayer = temp;
+                console.log(checkIfGameOver());
+                gameOverDude();
+            });
+        }
+    }
+}
+
+function gameOverDude() {
+    if (checkIfGameOver() != "game not over") {
+        console.log("ITS OVER ANAKIN I HAVE THE HIGH GGROUND");
+    } else {
+        console.log("overdose on ketamine, I have");
+    }
+}
+
+startButton.addEventListener("click", () => {
+    squares.forEach((square) => {
+        square.className += " squareActive";
+    });
+
+    if (aiCheck.checked) {
+        var { player1, player2 } = PVPorPVE("PVE");
+        console.log(checkIfGameOver());
+        if (checkIfGameOver() === "game not over") {
+            stuff(player1, player2);
+        }
+    } else {
+        var { player1, player2 } = PVPorPVE("PVP");
+        console.log(checkIfGameOver());
+        if (checkIfGameOver() === "game not over") {
+            stuff(player1, player2);
+        }
+    }
+});
+
+// ☭
--- modules/game.js
+++ modules/game.js
@@ -17,16 +17,12 @@ Some quick notes:
         - make moves 
         - utilise the minmax algorithm to find best moves to make  
 */
-import compPlayer from "./ai.js"
+import compPlayer from "./ai.js";
 
 const game = () => {
-    let board = [
-        "e", "e", "e",
-        "e", "e", "e",
-        "e", "e", "e",
-    ];  // e for empty, change each to X or O depending on how game transpires 
+    let board = ["e", "e", "e", "e", "e", "e", "e", "e", "e"]; // e for empty, change each to X or O depending on how game transpires
 
-    //if any of these cordinates are filled by either player game is over 
+    //if any of these cordinates are filled by either player game is over
     const winConditions = [
         [0, 1, 2],
         [3, 4, 5],
@@ -36,125 +32,119 @@ const game = () => {
         [2, 5, 8],
         [0, 4, 8],
         [2, 4, 6],
-    ]
+    ];
 
     const createPlayer = (type, marker, number) => {
-        return {type, marker, number} ; 
-    }
+        return { type, marker, number };
+    };
 
     const PVPorPVE = (input) => {
+        console.log("PVP or PVE is running!");
         /* Checks to see if the user would like to play against a human or 
         against an AI. Then creates the appropriate players needed.*/
-        if(input == "PVP"){
-            const player1 = createPlayer("player", "X", 1)
-            const player2 = createPlayer("player", "O", 2)
-            return{player1, player2} ; 
-        } else if(input == "PVE"){
-            const player1 = createPlayer("player", "X", 1) ;
-            const player2 = createPlayer("computer", "O", 2) ;
-            return {player1, player2} ; 
+        if (input == "PVP") {
+            const player1 = createPlayer("player", "X", 1);
+            const player2 = createPlayer("player", "O", 2);
+            return { player1, player2 };
+        } else if (input == "PVE") {
+            const player1 = createPlayer("player", "X", 1);
+            const player2 = createPlayer("computer", "O", 2);
+            return { player1, player2 };
         }
-    }  
-
-    //tightly coupled with turn() and turnClick()
-    const startGame = (divContainer, input) => {
-        board = [
-            "e", "e", "e",
-            "e", "e", "e",
-            "e", "e", "e",
-        ];  // hmmm is this necessary?
-        let {player1, player2} = PVPorPVE(input) ; 
-        turn(player1, player2, divContainer) ; 
-        console.log(divContainer)
-    }
+    };
 
     // tightly coupled with turn() and startGame()
-    const turnClick = (marker, divContainer) =>{
-        for(let i=0; i < divContainer.length ; i++){
-            if(board[i] == "e"){    
-                divContainer[i].addEventListener("click", (sqaure) =>{
-                    sqaure.innerHTML = marker ; 
-                    if(marker == "X"){
-                        board[i] = "X"; 
-                    } else if(marker == "O"){
-                        board[i] = "O" ; 
-                    }
-                })
+    const turnClick = (marker, divContainer) => {
+        console.log("turnClick");
+        if (board[i] == "e") {
+            divContainer.innerHTML = marker; // X or O
+            if (marker == "X") {
+                board[i] = "X";
+            } else if (marker == "O") {
+                board[i] = "O";
             }
         }
-    }
+    };
 
     // tightly coupled with startGame() and turnClick()
-    const turn = (player, altPlayer, divContainer) =>{
-        if(checkIfGameOver()!="game not over"){
-            console.log(checkIfGameOver())
-            return checkIfGameOver() ; 
-        } else{
-            console.log(divContainer)
-            turnClick(player, divContainer) ; 
-            turn(altPlayer, player, divContainer) ; 
+    const turn = (player, altPlayer, divContainer) => {
+        if (checkIfGameOver() !== "game not over") {
+            console.log(checkIfGameOver());
+            return checkIfGameOver();
+        } else {
+            turnClick(player.marker, divContainer);
+            console.log(divContainer);
+            debugger;
+            turn(altPlayer, player, divContainer);
         }
-    }
+    };
 
     const checkIfGameOver = () => {
         //define flags for each outcome
         let player1WinCheck = false;
         let player2WinCheck = false;
-        let drawWinCheck = false;   // need this to be true AND no empty spaces
+        let drawWinCheck = false; // need this to be true AND no empty spaces
         let emptySpaceCheck = false; // 1:  there is empty spaces 0: there is not
 
-        // for loop checks to see if there is any empty spaces (ie game still running 
+        // for loop checks to see if there is any empty spaces (ie game still running
         // if none of win conditions are met )
         for (let i = 0; i < board.length; i++) {
             if (board[i] == "e") {
-                console.log(board[i])
+                console.log(board[i]);
                 emptySpaceCheck = true;
             }
         }
         // check the indicies of each win condition against the board
         winConditions.forEach((winCond) => {
-            if (board[winCond[0]] == "X" && board[winCond[1]] == "X" && board[winCond[2]] == "X") {
+            if (
+                board[winCond[0]] == "X" &&
+                board[winCond[1]] == "X" &&
+                board[winCond[2]] == "X"
+            ) {
                 player1WinCheck = true;
-            } else if (board[winCond[0]] == "O" && board[winCond[1]] == "O" && board[winCond[2]] == "O") {
-                player2WinCheck = false;
+            } else if (
+                board[winCond[0]] == "O" &&
+                board[winCond[1]] == "O" &&
+                board[winCond[2]] == "O"
+            ) {
+                player2WinCheck = true;
             } else {
                 drawWinCheck = true;
             }
-        })
-        // return the results 
-        if(player1WinCheck == true){
-            return "player 1 won" ;
-        } else if(player2WinCheck == true){
-            return "player 2 won" ; 
-        } else if(drawWinCheck == true && emptySpaceCheck == false){
-            return "it was a draw" ; 
-        } else{
-            return "game not over" ; 
+        });
+        // return the results
+        if (player1WinCheck == true) {
+            return "player 1 won";
+        } else if (player2WinCheck == true) {
+            return "player 2 won";
+        } else if (drawWinCheck == true && emptySpaceCheck == false) {
+            return "it was a draw";
+        } else {
+            return "game not over";
         }
-    }
-
+    };
 
     const emptyIndicies = () => {
         // returns array with indicies of empty spots
-        let indexArr = [] ;  
-        for(let i = 0 ; i < board.length ; i++){
-            if(board[i]=="e"){
-                indexArr.push(i) ; 
-            }             
+        let indexArr = [];
+        for (let i = 0; i < board.length; i++) {
+            if (board[i] == "e") {
+                indexArr.push(i);
+            }
         }
-        return indexArr ; 
-    }
+        return indexArr;
+    };
 
-    return{
-            board, 
-            winConditions,
-            PVPorPVE, 
-            startGame,
-            turnClick, 
-            turn, 
-            checkIfGameOver, 
-            emptyIndicies
-        }
-}
+    return [
+        board,
+        winConditions,
+        PVPorPVE,
+        startGame,
+        turnClick,
+        turn,
+        checkIfGameOver,
+        emptyIndicies,
+    ];
+};
 
-export default game 
+export default game;
